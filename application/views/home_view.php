5aa8a3b94b39ddf0a2c0e55943f951162bca7008
alik <anothermalik@gmail.com>
2018-02-14 00:11:03 +0700

    tambah sponsor, footer

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,172 +1,306 @@
 <!DOCTYPE html>
 <html>
-    <head>
-	    <meta charset="utf-8">
-	    <meta content="IE=edge" http-equiv="X-UA-Compatible">
-	    <meta content="width=device-width, initial-scale=1" name="viewport">
 
-	    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	    <title>Marketer - Responsive HTML5 Template</title>
+<head>
+	<meta charset="utf-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
 
-	    <!-- web-fonts -->
-	    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,500" rel="stylesheet" type="text/css">
-	    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Marketer - Responsive HTML5 Template</title>
 
-	    <!-- off-canvas -->
-	    <link href="<?php echo base_url() ?>asset/css/mobile-menu.css" rel="stylesheet">
+	<!-- web-fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,500" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 
-	    <!-- font-awesome -->
-	    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+	<!-- off-canvas -->
+	<link href="<?php echo base_url() ?>asset/css/mobile-menu.css" rel="stylesheet">
 
-	    <!-- Flat Icon -->
-	    <link href="<?php echo base_url() ?>asset/fonts/flaticon/flaticon.css" rel="stylesheet">
+	<!-- font-awesome -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 
-	    <!-- Bootstrap -->
-	    <link crossorigin="anonymous" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" rel="stylesheet">
+	<!-- Flat Icon -->
+	<link href="<?php echo base_url() ?>asset/fonts/flaticon/flaticon.css" rel="stylesheet">
 
-	    <!-- Style CSS -->
-	    <link href="<?php echo base_url() ?>asset/css/style.css" rel="stylesheet">
-  
- 	</head>
-    <body>
-        <div id="main-wrapper">
-            <div class="content-wrapper">
-                <nav class="navbar m-menu navbar-default">
-                    <div class="container">
-                        <div class="navbar-header">
-                            <button class="navbar-toggle collapsed" data-target="#navbar-collapse-1" data-toggle="collapse" type="button">
-                                
-                            </button>
-                            <a class="navbar-brand" href="index.html">
-                                MJM TRAVEL
-                        </div>
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="collapse navbar-collapse" id="#navbar-collapse-1">
-                            <ul class="nav navbar-nav navbar-right main-nav">
-                                <li class="active">
-                                    <a href="index.html">
-                                        Home
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="about.html">
-                                        Paket
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="services.html">
-                                        Mengapa MJM
-                                    </a>
-                                </li>
-                               
-                            </ul>
-                        </div>
-                        <!-- .navbar-collapse -->
-                    </div>
-                </nav>
-                <!-- end of nav -->
+	<!-- Bootstrap -->
+	<link crossorigin="anonymous" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+	rel="stylesheet">
 
-                <div class="carousel slide hero-slide hidden-xs" data-ride="carousel" id="my-carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li class="active" data-slide-to="0" data-target="#my-carousel">
-                        </li>
-                    </ol>
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                            <img alt="Hero Slide" src="<?php echo base_url() ?>asset/img/hero-slide-1.jpg">
-                                <div class="carousel-caption">
-                                    <h1>
-                                        Strategic Management
-                                    </h1>
-                                    <p>
-                                        Efficiently develop parallel e-markets through impactful outsourcing.<br/>Conveniently drive prospective functionalities before.
-                                    </p>
-                                </div>
-                            </img>
-                        </div>
-                    </div>
-                </div>
-                <!-- end of carousel -->
+	<!-- Style CSS -->
+	<link href="<?php echo base_url() ?>asset/css/style.css" rel="stylesheet">
 
-                <section class="featured-box">
-    				<div class="container">
-        				<div class="row">
-        					<div class="col-md-12">
-            					<div class="featured-content-wrapper">
-            						<div class="featured-img">
-                						<img class="img-responsive" src="<?php echo base_url() ?>asset/img/kabah.jpg" style="height: 250px; padding-top: 20px; padding-bottom: 20px">
-            						</div>
-            						<div class="featured-content">
-                						<h1>Paket ter favorit bulan ini </h1>
-                						<p>
-                							Beribaah kerumah sang pencipta itu hal yang sangat berkesan dalam diri kita sendiri apalagi menjalankannya dengan liburan ke negara tetangganya yaitu turkey.
-                						</p>
-                						<a href="" class="btn btn-default btn-lg">Learn More About Us</a>
-            						</div>
-            					</div>
-            				</div>
-        				</div>
-    				</div>
-				</section>
-				<!-- /.featured-box -->
+</head>
 
-                <section class="case-studies">
-    				<div class="container">
-        				<div class="section-title">
-            				<h1>Layanan Kami</h1>
-            				<p>Dapatkan perjalanan sesuai dengan kebutuhan anda.</p>
-        				</div>
-        				<div class="row text-center">
-            				<div class="col-md-4">
-                				<div class="left-icon-wraper">
-                    				<div class="icon">
-                        				<img src="https://jannahtravel.co.id/assets/front/images/umrah.png" style="width: 100px;height: 100px">
-                    				</div>
-                    				<div class="content">
-                    					<h2>Layanan Haji</h2>
-                    				</div>
-                				</div>
-            				</div>
-            				<div class="col-md-4">
-                				<div class="left-icon-wraper">
-                					<div class="icon">
-                						<img src="https://jannahtravel.co.id/assets/front/images/umrah.png" style="width: 100px;height: 100px">
-                					</div>
-                					<div class="content">
-                    					<h2>Layanan Umrah</h2>
-                					</div>
-                				</div>
-            				</div>
-            				<div class="col-md-4">
-                				<div class="left-icon-wraper">
-                					<div class="icon">
-                						<img src="https://jannahtravel.co.id/assets/front/images/umrah.png" style="width: 100px;height: 100px">
-                					</div>
-                					<div class="content">
-                     					<h2>Layanan Tour</h2>
-                					</div>
-                				</div>
-            				</div>
-        				</div><!-- /.row -->
-    				</div><!-- /.container -->
-				</section>
-				<!-- /.client-logo -->
+<body>
+	<div id="main-wrapper">
+		<div class="content-wrapper">
+			<nav class="navbar m-menu navbar-default">
+				<div class="container">
+					<div class="navbar-header">
+						<button class="navbar-toggle collapsed" data-target="#navbar-collapse-1" data-toggle="collapse" type="button">
 
-            </div>
-        </div>
-       
-        <!-- Script -->
-        <script src="<?php echo base_url() ?>asset/js/jquery-2.1.4.min.js">
-        </script>
-        <script crossorigin="anonymous" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
-        </script>
-        <script src="<?php echo base_url() ?>asset/js/smoothscroll.js">
-        </script>
-        <script src="<?php echo base_url() ?>asset/js/mobile-menu.js">
-        </script>
-        <script src="<?php echo base_url() ?>asset/js/scripts.js">
-        </script>
-    </body>
+						</button>
+						<a class="navbar-brand" href="index.html">
+							MJM TRAVEL
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="#navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right main-nav">
+							<li class="active">
+								<a href="index.html">
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="about.html">
+									Paket
+								</a>
+							</li>
+							<li>
+								<a href="services.html">
+									Mengapa MJM
+								</a>
+							</li>
+
+						</ul>
+					</div>
+					<!-- .navbar-collapse -->
+				</div>
+			</nav>
+			<!-- end of nav -->
+
+			<div class="carousel slide hero-slide hidden-xs" data-ride="carousel" id="my-carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li class="active" data-slide-to="0" data-target="#my-carousel">
+					</li>
+				</ol>
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner" role="listbox">
+					<div class="item active">
+						<img alt="Hero Slide" src="<?php echo base_url() ?>asset/img/hero-slide-1.jpg">
+						<div class="carousel-caption">
+							<h1>
+								Strategic Management
+							</h1>
+							<p>
+								Efficiently develop parallel e-markets through impactful outsourcing.
+								<br/>Conveniently drive prospective functionalities before.
+							</p>
+						</div>
+						</img>
+					</div>
+				</div>
+			</div>
+			<!-- end of carousel -->
+
+			<section class="featured-box">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="featured-content-wrapper">
+								<div class="featured-img">
+									<img class="img-responsive" src="<?php echo base_url() ?>asset/img/kabah.jpg" style="height: 250px; padding-top: 20px; padding-bottom: 20px">
+								</div>
+								<div class="featured-content">
+									<h1>Paket ter favorit bulan ini </h1>
+									<p>
+										Beribaah kerumah sang pencipta itu hal yang sangat berkesan dalam diri kita sendiri apalagi menjalankannya dengan liburan
+										ke negara tetangganya yaitu turkey.
+									</p>
+									<a href="" class="btn btn-default btn-lg">Learn More About Us</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- /.featured-box -->
+
+			<section class="case-studies">
+				<div class="container">
+					<div class="section-title">
+						<h1>Layanan Kami</h1>
+						<p>Dapatkan perjalanan sesuai dengan kebutuhan anda.</p>
+					</div>
+					<div class="row text-center">
+						<div class="col-md-4">
+							<div class="left-icon-wraper">
+								<div class="icon">
+									<img src="https://jannahtravel.co.id/assets/front/images/umrah.png" style="width: 100px;height: 100px">
+								</div>
+								<div class="content">
+									<h2>Layanan Haji</h2>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="left-icon-wraper">
+								<div class="icon">
+									<img src="https://jannahtravel.co.id/assets/front/images/umrah.png" style="width: 100px;height: 100px">
+								</div>
+								<div class="content">
+									<h2>Layanan Umrah</h2>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="left-icon-wraper">
+								<div class="icon">
+									<img src="https://jannahtravel.co.id/assets/front/images/umrah.png" style="width: 100px;height: 100px">
+								</div>
+								<div class="content">
+									<h2>Layanan Tour</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- /.row -->
+				</div>
+				<!-- /.container -->
+			</section>
+			<!-- /.client-logo -->
+
+			<section class="client-logo ptb-100">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2 col-sm-4 col-xs-6 section-margin">
+							<a href="#">
+								<img src="<?php echo base_url() ?>asset/img/logo-client-1.jpg" alt="Image">
+							</a>
+						</div>
+						<div class="col-md-2 col-sm-4 col-xs-6 section-margin">
+							<a href="#">
+								<img src="<?php echo base_url() ?>asset/img/logo-client-2.jpg" alt="Image">
+							</a>
+						</div>
+						<div class="col-md-2 col-sm-4 col-xs-6 section-margin">
+							<a href="#">
+								<img src="<?php echo base_url() ?>asset/img/logo-client-3.jpg" alt="Image">
+							</a>
+						</div>
+						<div class="col-md-2 col-sm-4 col-xs-6 section-margin">
+							<a href="#">
+								<img src="<?php echo base_url() ?>asset/img/logo-client-4.jpg" alt="Image">
+							</a>
+						</div>
+						<div class="col-md-2 col-sm-4 col-xs-6 section-margin">
+							<a href="#">
+								<img src="<?php echo base_url() ?>asset/img/logo-client-5.jpg" alt="Image">
+							</a>
+						</div>
+						<div class="col-md-2 col-sm-4 col-xs-6 section-margin">
+							<a href="#">
+								<img src="<?php echo base_url() ?>asset/img/logo-client-6.jpg" alt="Image">
+							</a>
+						</div>
+					</div>
+				</div>
+				<!--end of .container -->
+			</section>
+			<!-- /.client-logo -->
+
+			<footer class="footer">
+
+				<!-- Footer Widget Section -->
+				<div class="footer-widget-section">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="footer-logo">
+									<a href="#">
+										<img class="img-responsive" src="img/logo-footer.png" alt="">
+									</a>
+								</div>
+							</div>
+							<!-- /.col-sm-4 -->
+
+							<div class="col-md-8">
+								<div class="footer-text">
+									<p>Assertively architect synergistic platforms vis-a-vis premium results. Synergistically facilitate high-payoff e-services
+										rather than top-line sources. Collaboratively drive market positioning infomediaries without real-time e-commerce.</p>
+
+									<ul>
+										<li class="phone">
+											<i class="fa fa-volume-control-phone" aria-hidden="true"></i> 1-677-124-44227
+											<small>
+												<i class="fa fa-clock-o" aria-hidden="true"></i> Mon - Sat 8.00 - 18.00h</small>
+										</li>
+										<li class="address">
+											<address>
+												<i class="fa fa-map-pin" aria-hidden="true"></i>
+												437 S Olive St, Los Angeles
+											</address>
+										</li>
+									</ul>
+
+								</div>
+							</div>
+							<!-- /.col-sm-4 -->
+						</div>
+					</div>
+				</div>
+				<!-- /.Footer Widget Section -->
+
+				<div class="copyright-section">
+					<div class="container clearfix">
+						<span class="copytext">Copyright &copy; 2016 |
+							<a href="https://uicookies.com/downloads/marketer-bootstrap-html5-advertising-agency-website-template/">Marketer</a> Designed And Developed By:
+							<strong style="color:#4d6de3">uiCookies.com</strong>
+						</span>
+
+						<ul class="list-inline pull-right">
+							<li class="active">
+								<a href="#">Home</a>
+							</li>
+							<li>
+								<a href="#">About</a>
+							</li>
+							<li>
+								<a href="#">Services</a>
+							</li>
+							<li>
+								<a href="#">Portfolio</a>
+							</li>
+							<li>
+								<a href="#">Blog</a>
+							</li>
+							<li>
+								<a href="#">Contact</a>
+							</li>
+						</ul>
+					</div>
+					<!-- .container -->
+
+				</div>
+				<!-- .copyright-section -->
+			</footer>
+			<!-- .footer -->
+
+		</div>
+	</div>
+
+	<!-- Script -->
+	<script src="<?php echo base_url() ?>asset/js/jquery-2.1.4.min.js">
+
+
+	</script>
+	<script crossorigin="anonymous" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
+
+
+	</script>
+	<script src="<?php echo base_url() ?>asset/js/smoothscroll.js">
+
+
+	</script>
+	<script src="<?php echo base_url() ?>asset/js/mobile-menu.js">
+
+
+	</script>
+	<script src="<?php echo base_url() ?>asset/js/scripts.js">
+
+
+	</script>
+</body>
+
 </html>
