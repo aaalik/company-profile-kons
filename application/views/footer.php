4cd4e49b89b1c83af65096caaadfdc3bdb0b8529
Haris Manaf <harismanaf96@gmail.com>
2018-02-14 22:55:21 +0700

    paket coba

---
+++ application/views/footer.php
@@ -0,0 +1,85 @@
+<footer class="footer">
+
+				<!-- Footer Widget Section -->
+				<div class="footer-widget-section">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="footer-logo">
+									<a href="#">
+										<img class="img-responsive" src="img/logo-footer.png" alt="">
+									</a>
+								</div>
+							</div>
+							<!-- /.col-sm-4 -->
+
+							<div class="col-md-8">
+								<div class="footer-text">
+									<p>Assertively architect synergistic platforms vis-a-vis premium results. Synergistically facilitate high-payoff e-services
+										rather than top-line sources. Collaboratively drive market positioning infomediaries without real-time e-commerce.</p>
+
+									<ul>
+										<li class="phone">
+											<i class="fa fa-volume-control-phone" aria-hidden="true"></i> 1-677-124-44227
+											<small>
+												<i class="fa fa-clock-o" aria-hidden="true"></i> Mon - Sat 8.00 - 18.00h</small>
+										</li>
+										<li class="address">
+											<address>
+												<i class="fa fa-map-pin" aria-hidden="true"></i>
+												437 S Olive St, Los Angeles
+											</address>
+										</li>
+									</ul>
+
+								</div>
+							</div>
+							<!-- /.col-sm-4 -->
+						</div>
+					</div>
+				</div>
+				<!-- /.Footer Widget Section -->
+
+				<div class="copyright-section">
+					<div class="container clearfix">
+						<span class="copytext">Copyright &copy; 2016 |
+							<a href="https://uicookies.com/downloads/marketer-bootstrap-html5-advertising-agency-website-template/">Marketer</a> Designed And Developed By:
+							<strong style="color:#4d6de3">uiCookies.com</strong>
+						</span>
+
+					
+					</div>
+					<!-- .container -->
+
+				</div>
+				<!-- .copyright-section -->
+			</footer>
+			<!-- .footer -->
+
+		</div>
+	</div>
+
+	<!-- Script -->
+	<script src="<?php echo base_url() ?>asset/js/jquery-2.1.4.min.js">
+
+
+	</script>
+	<script crossorigin="anonymous" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
+
+
+	</script>
+	<script src="<?php echo base_url() ?>asset/js/smoothscroll.js">
+
+
+	</script>
+	<script src="<?php echo base_url() ?>asset/js/mobile-menu.js">
+
+
+	</script>
+	<script src="<?php echo base_url() ?>asset/js/scripts.js">
+
+
+	</script>
+</body>
+
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Marketer - Responsive HTML5 Template</title>
+
+	<!-- web-fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,500" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+	<!-- off-canvas -->
+	<link href="<?php echo base_url() ?>asset/css/mobile-menu.css" rel="stylesheet">
+
+	<!-- font-awesome -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Flat Icon -->
+	<link href="<?php echo base_url() ?>asset/fonts/flaticon/flaticon.css" rel="stylesheet">
+
+	<!-- Bootstrap -->
+	<link crossorigin="anonymous" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+	rel="stylesheet">
+
+	<!-- Style CSS -->
+    <link href="<?php echo base_url() ?>asset/css/style.css" rel="stylesheet">
+    <!--css paket -->
+    
+
+</head>
+
+<body>
+	<div id="main-wrapper">
+		<div class="content-wrapper">
+			<nav class="navbar m-menu navbar-default">
+				<div class="container">
+					<div class="navbar-header">
+						<button class="navbar-toggle collapsed" data-target="#navbar-collapse-1" data-toggle="collapse" type="button">
+
+						</button>
+						<a class="navbar-brand" href="index.html">
+							MJM TRAVEL
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="#navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right main-nav">
+							<li class="active">
+								<a href="<?php echo base_url()?>home_view.php">
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo base_url()?>index.php/paket">
+									Paket
+								</a>
+							</li>
+							<li>
+								<a href="services.html">
+									Mengapa MJM
+								</a>
+							</li>
+
+						</ul>
+					</div>
+					<!-- .navbar-collapse -->
+				</div>
+			</nav>
+			<!-- end of nav -->
