5f5bbc1e2949545159cd05648599aed6ba12efbe
Haris Manaf <harismanaf96@gmail.com>
2018-02-15 02:43:47 +0700

    tampilan paket

--- application/views/paket.php
+++ application/views/paket.php
@@ -1,14 +1,121 @@
-<div class="row">
-<div class="col-md-4 ">
-  <div class="card h-100">
-    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-    <div class="card-body">
-      <h4 class="card-title">
-        <a href="#">Project One</a>
-      </h4>
-      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    		<!-- Indicators -->
+    		<ol class="carousel-indicators">
+     		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      		<li data-target="#myCarousel" data-slide-to="1"></li>
+      		<li data-target="#myCarousel" data-slide-to="2"></li>
+    		</ol>
+
+    		<!-- Wrapper for slides -->
+    <div class="carousel-inner">
+       <div class="item active">
+          <img src="<?php echo base_url()?>asset/img/hero-slide-1.jpg" alt="Los Angeles" style="width:100%;">
+         </div>
+
+         <div class="item">
+           <img src="<?php echo base_url()?>asset/img/hero-slide-2.jpg" alt="Chicago" style="width:100%;">
+         </div>
+    
+         <div class="item">
+           <img src="<?php echo base_url()?>asset/img/hero-slide-3.jpg" alt="New york" style="width:100%;">
+         </div>
+       </div>
+
+       <!-- Left and right controls -->
+       <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+         <span class="glyphicon glyphicon-chevron-left"></span>
+         <span class="sr-only">Previous</span>
+       </a>
+       <a class="right carousel-control" href="#myCarousel" data-slide="next">
+         <span class="glyphicon glyphicon-chevron-right"></span>
+         <span class="sr-only">Next</span>
+       </a>
     </div>
-  </div>
+</div>	
+<br>	
+<br>	
+
+<div class="container">	
+
+   	<div class="col-md-12">
+		<h3>Paket-paket</h3>
+	</div>
+
+	<div class="row">
+		<div class="col-md-4 ">
+	  		<div class="card h-100">
+	    		<a href="#"><img class="card-img-top" style="width: 350px" src="http://placehold.it/700x400" alt=""></a>
+	    		<div class="card-body">
+	      		<h4 class="card-title">
+	        		<a href="#">Project One</a>
+	      		</h4>
+	      		<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+	    		</div>
+	  		</div>
+		</div>
+
+		<div class="col-md-4 ">
+	  		<div class="card h-100">
+	    		<a href="#"><img class="card-img-top" style="width: 350px" src="http://placehold.it/700x400" alt=""></a>
+	    		<div class="card-body">
+	      		<h4 class="card-title">
+	        		<a href="#">Project One</a>
+	      		</h4>
+	      		<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+	    		</div>
+	  		</div>
+		</div>
+
+		<div class="col-md-4 ">
+	  		<div class="card h-100">
+	    		<a href="#"><img class="card-img-top" style="width: 350px" src="http://placehold.it/700x400" alt=""></a>
+	    		<div class="card-body">
+	      		<h4 class="card-title">
+	        		<a href="#">Project One</a>
+	      		</h4>
+	      		<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+	    		</div>
+	  		</div>
+		</div>
+
+		<div class="col-md-4 ">
+	  		<div class="card h-100">
+	    		<a href="#"><img class="card-img-top" style="width: 350px" src="http://placehold.it/700x400" alt=""></a>
+	    		<div class="card-body">
+	      		<h4 class="card-title">
+	        		<a href="#">Project One</a>
+	      		</h4>
+	      		<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+	    		</div>
+	  		</div>
+		</div>
+
+		<div class="col-md-4 ">
+	  		<div class="card h-100">
+	    		<a href="#"><img class="card-img-top" style="width: 350px" src="http://placehold.it/700x400" alt=""></a>
+	    		<div class="card-body">
+	      		<h4 class="card-title">
+	        		<a href="#">Project One</a>
+	      		</h4>
+	      		<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+	    		</div>
+	  		</div>
+		</div>
+
+		<div class="col-md-4 ">
+	  		<div class="card h-100">
+	    		<a href="#"><img class="card-img-top" style="width: 350px" src="http://placehold.it/700x400" alt=""></a>
+	    		<div class="card-body">
+	      		<h4 class="card-title">
+	        		<a href="#">Project One</a>
+	      		</h4>
+	      		<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+	    		</div>
+	  		</div>
+		</div>
+
+
+
+	</div>
 </div>
 
-</div>
